3f827c29a72276b505c550e2a8e0c205402ba564
Joel Alexandre <alexkhang25@yahoo.fr>
2017-03-06 13:57:37 +0100

    "final version of the weather App"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,26 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AddWeatherPage} from '../pages/add-weather/add-weather'; 
+import { AddWeatherPage} from '../pages/add-weather/add-weather';
 import { ForecastPage } from '../pages/forecast/forecast';
-import { MapPage } from '../pages/map/map'; 
-import { TaskPage } from '../pages/task/task'; 
-import { ConvertTemperature } from '../pipes/convert-temperature'; 
+import { MapPage } from '../pages/map/map';
+import { TaskPage } from '../pages/task/task';
+import { ConvertTemperature } from '../pipes/convert-temperature';
 import { Weather } from '../providers/weather';
 import { StorageService } from '../providers/storage-service';
 import { WeatherComponent } from '../components/weather/weather';
 import { Storage } from '@ionic/storage';
 import { PlacesPage } from '../pages/places/places';
-import { SharePage } from '../pages/share/share'; 
+import { SharePage } from '../pages/share/share';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage, 
-    AddWeatherPage,  
-    ConvertTemperature, 
+    HomePage,
+    AddWeatherPage,
+    ConvertTemperature,
     ForecastPage,
-    WeatherComponent, 
+    WeatherComponent,
     PlacesPage,
     MapPage,
     SharePage,
@@ -33,9 +33,9 @@ import { SharePage } from '../pages/share/share';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,  
-    AddWeatherPage, 
-    ForecastPage, 
+    HomePage,
+    AddWeatherPage,
+    ForecastPage,
     TaskPage,
     PlacesPage,
     MapPage,
